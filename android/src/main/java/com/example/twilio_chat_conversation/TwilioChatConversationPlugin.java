8804d548035100a53b93b21da7a8b7aedec26a6a
hrvoje <hrvoje.cukman@gmail.com>
2024-03-26 14:38:38 +0100

    Potential Andorid fixes

--- android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
+++ android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
@@ -23,7 +23,7 @@ import io.flutter.plugin.common.EventChannel.StreamHandler;
 /**
  * TwilioChatConversationPlugin
  */
-public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler, StreamHandler {
+public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
