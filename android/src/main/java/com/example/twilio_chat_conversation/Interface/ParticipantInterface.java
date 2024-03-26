2b2c0fa8bff7e917b3d6f7d362a3a810cb39a6f3
hrvoje <hrvoje.cukman@gmail.com>
2024-03-26 14:35:34 +0100

    Potential Andorid fixes

--- android/src/main/java/com/example/twilio_chat_conversation/Interface/ParticipantInterface.java
+++ android/src/main/java/com/example/twilio_chat_conversation/Interface/ParticipantInterface.java
@@ -1,7 +1,5 @@
 package com.example.twilio_chat_conversation.Interface;
 
-import java.util.Map;
-
 public interface ParticipantInterface {
     default void onParticipantAdded(String conversationSid, String participantIdentity) {
     }
--- android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
+++ android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
@@ -10,6 +10,7 @@ import com.example.twilio_chat_conversation.Utility.Methods;
 
 import java.util.List;
 import java.util.Map;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
@@ -19,205 +20,195 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.EventChannel.EventSink;
 import io.flutter.plugin.common.EventChannel.StreamHandler;
 
-/** TwilioChatConversationPlugin */
-public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler , StreamHandler , MessageInterface, AccessTokenInterface, ParticipantInterface {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private EventChannel eventChannel;
-  private EventChannel tokenEventChannel;
-  private EventChannel participantsEventChannel;
-  private EventChannel.EventSink eventSink;
-  private EventChannel.EventSink tokenEventSink;
-  private EventChannel.EventSink participantsEventSink;
-
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation");
-    channel.setMethodCallHandler(this);
-
-    // Initialize and set StreamHandlers for each EventChannel
-    eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onMessageUpdated");
-    eventChannel.setStreamHandler(new MessageUpdateStreamHandler());
-
-    tokenEventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onTokenStatusChange");
-    tokenEventChannel.setStreamHandler(new TokenStatusChangeStreamHandler());
-
-    participantsEventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onParticipantUpdate");
-    participantsEventChannel.setStreamHandler(new ParticipantUpdateStreamHandler());
-
-    ConversationHandler.flutterPluginBinding = flutterPluginBinding;
-  }
-
-  private static class MessageUpdateStreamHandler implements StreamHandler {
-    private EventSink eventSink;
+/**
+ * TwilioChatConversationPlugin
+ */
+public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler, StreamHandler, MessageInterface, AccessTokenInterface, ParticipantInterface {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private EventChannel eventChannel;
+    private EventChannel tokenEventChannel;
+    private EventChannel participantsEventChannel;
+    private EventChannel.EventSink eventSink;
+    private EventChannel.EventSink tokenEventSink;
+    private EventChannel.EventSink participantsEventSink;
+    private ConversationHandler conversationHandler = new ConversationHandler();
 
     @Override
-    public void onListen(Object arguments, EventSink events) {
-      this.eventSink = events;
-      // Setup listener that will use this.eventSink to send message updates
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation");
+        channel.setMethodCallHandler(this);
+
+        // Initialize and set StreamHandlers for each EventChannel
+        eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onMessageUpdated");
+        eventChannel.setStreamHandler(new MessageUpdateStreamHandler());
+
+        tokenEventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onTokenStatusChange");
+        tokenEventChannel.setStreamHandler(new TokenStatusChangeStreamHandler());
+
+        participantsEventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "twilio_chat_conversation/onParticipantUpdate");
+        participantsEventChannel.setStreamHandler(new ParticipantUpdateStreamHandler());
+
+        ConversationHandler.flutterPluginBinding = flutterPluginBinding;
+    }
+
+    private class MessageUpdateStreamHandler implements StreamHandler {
+        private EventSink eventSink;
+
+        @Override
+        public void onListen(Object arguments, EventSink events) {
+            this.eventSink = events;
+            conversationHandler.setListener(new MessageInterface() {
+                @Override
+                public void onMessageUpdate(Map<String, Object> message) {
+                    if (eventSink != null) {
+                        eventSink.success(message);
+                    }
+                }
+
+                @Override
+                public void onTypingUpdate(boolean isTyping) {
+                    if (eventSink != null) {
+                        eventSink.success(isTyping);
+                    }
+                }
+
+            });
+        }
+
+        @Override
+        public void onCancel(Object arguments) {
+            this.eventSink = null;
+        }
+    }
+
+    // Separate StreamHandler for token status changes
+    private class TokenStatusChangeStreamHandler implements StreamHandler {
+        private EventSink eventSink;
+
+        @Override
+        public void onListen(Object arguments, EventSink events) {
+            this.eventSink = events;
+
+            conversationHandler.setTokenListener(new AccessTokenInterface() {
+                @Override
+                public void onTokenStatusChange(Map message) {
+                    /// Pass the message result back to the Flutter side
+                    if (eventSink != null) {
+                        eventSink.success(message);
+                    }
+                }
+
+            });
+        }
+
+
+        @Override
+        public void onCancel(Object arguments) {
+            this.eventSink = null;
+        }
+    }
+
+    // Separate StreamHandler for participant updates
+    private class ParticipantUpdateStreamHandler implements StreamHandler {
+        private EventSink eventSink;
+
+        @Override
+        public void onListen(Object arguments, EventSink events) {
+            this.eventSink = events;
+
+            conversationHandler.setParticipantListener(new ParticipantInterface() {
+                @Override
+                public void onParticipantAdded(String conversationSid, String participantIdentity) {
+                    /// Pass the message result back to the Flutter side
+                    if (eventSink != null) {
+                        eventSink.success(participantIdentity);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onCancel(Object arguments) {
+            this.eventSink = null;
+        }
     }
 
     @Override
-    public void onCancel(Object arguments) {
-      this.eventSink = null;
-    }
-  }
-
-  // Separate StreamHandler for token status changes
-  private static class TokenStatusChangeStreamHandler implements StreamHandler {
-    private EventSink eventSink;
-
-    @Override
-    public void onListen(Object arguments, EventSink events) {
-      this.eventSink = events;
-      // Setup listener that will use this.eventSink to send token status changes
-    }
-
-    @Override
-    public void onCancel(Object arguments) {
-      this.eventSink = null;
-    }
-  }
-
-  // Separate StreamHandler for participant updates
-  private static class ParticipantUpdateStreamHandler implements StreamHandler {
-    private EventSink eventSink;
-
-    @Override
-    public void onListen(Object arguments, EventSink events) {
-      this.eventSink = events;
-    }
-
-    @Override
-    public void onCancel(Object arguments) {
-      this.eventSink = null;
-    }
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    System.out.println("call.method->"+call.method);
-    switch (call.method) {
-      // To generate twilio access token #
-      case Methods.generateToken: //Generate token and authenticate user
-        String accessToken = ConversationHandler.generateAccessToken(call.argument("accountSid"),call.argument("apiKey"),call.argument("apiSecret"),call.argument("identity"),call.argument("serviceSid"));
-        System.out.println("accessToken generated->"+accessToken);
-        result.success(accessToken);
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        System.out.println("call.method->" + call.method);
+        switch (call.method) {
+            // To generate twilio access token #
+            case Methods.generateToken: //Generate token and authenticate user
+                String accessToken = ConversationHandler.generateAccessToken(call.argument("accountSid"), call.argument("apiKey"), call.argument("apiSecret"), call.argument("identity"), call.argument("serviceSid"));
+                System.out.println("accessToken generated->" + accessToken);
+                result.success(accessToken);
 //        ConversationHandler.initializeConversationClient(accessToken,result);
-        break;
+                break;
 
-      case Methods.initializeConversationClient: //Generate token and authenticate user
-        ConversationHandler.initializeConversationClient(call.argument("accessToken"),result);
-        break;
+            case Methods.initializeConversationClient: //Generate token and authenticate user
+                ConversationHandler.initializeConversationClient(call.argument("accessToken"), result);
+                break;
 
-      // Create new conversation #
-      case Methods.createConversation:
-        ConversationHandler.createConversation(call.argument("conversationName"),call.argument("identity"),result);
-        break;
-      // Get list of conversations for logged in user #
-      case Methods.getConversations:
-        List<Map<String, Object>> conversationList = ConversationHandler.getConversationsList();
-        result.success(conversationList);
-        break;
-      // Get messages from the specific conversation #
-      case Methods.getMessages:
-        ConversationHandler.getAllMessages(call.argument("conversationSid"),call.argument("messageCount"),result);
-        break;
-      //Join the existing conversation #
-      case Methods.joinConversation:
-        String joinStatus = ConversationHandler.joinConversation(call.argument("conversationSid"));
-        result.success(joinStatus);
-        break;
-      // Send message #
-      case Methods.sendMessage:
-        ConversationHandler.sendMessages(call.argument("message"),call.argument("conversationSid"), Boolean.TRUE.equals(call.argument("isFromChatGpt")),result);
-        break;
-      // Add participant in a conversation #
-      case Methods.addParticipant:
-        ConversationHandler.addParticipant(call.argument("participantName"),call.argument("conversationSid"),result);
-        break;
-      case Methods.removeParticipant:
-        ConversationHandler.removeParticipant(call.argument("participantName"),call.argument("conversationSid"),result);
-        break;
-      // Get & Listen messages from the specific conversation #
-      case Methods.receiveMessages:
-      case Methods.subscribeToMessageUpdate:
-        ConversationHandler.subscribeToMessageUpdate(call.argument("conversationSid"));
-        break;
-      // Get participants from the specific conversation #
-      case Methods.getParticipants:
-        ConversationHandler.getParticipants(call.argument("conversationSid"),result);
-        break;
-      case Methods.unSubscribeToMessageUpdate:
-        ConversationHandler.unSubscribeToMessageUpdate(call.argument("conversationSid"));
-        break;
-      case Methods.updateAccessToken:
-        ConversationHandler.updateAccessToken(call.argument("accessToken"),result);
-        break;
-      case Methods.sendTypingIndicator:
-        ConversationHandler.sendTypingIndicator(call.argument("conversationSid"),result);
-        break;
-      default:
-        break;
+            // Create new conversation #
+            case Methods.createConversation:
+                ConversationHandler.createConversation(call.argument("conversationName"), call.argument("identity"), result);
+                break;
+            // Get list of conversations for logged in user #
+            case Methods.getConversations:
+                List<Map<String, Object>> conversationList = ConversationHandler.getConversationsList();
+                result.success(conversationList);
+                break;
+            // Get messages from the specific conversation #
+            case Methods.getMessages:
+                ConversationHandler.getAllMessages(call.argument("conversationSid"), call.argument("messageCount"), result);
+                break;
+            //Join the existing conversation #
+            case Methods.joinConversation:
+                String joinStatus = ConversationHandler.joinConversation(call.argument("conversationSid"));
+                result.success(joinStatus);
+                break;
+            // Send message #
+            case Methods.sendMessage:
+                ConversationHandler.sendMessages(call.argument("message"), call.argument("conversationSid"), Boolean.TRUE.equals(call.argument("isFromChatGpt")), result);
+                break;
+            // Add participant in a conversation #
+            case Methods.addParticipant:
+                ConversationHandler.addParticipant(call.argument("participantName"), call.argument("conversationSid"), result);
+                break;
+            case Methods.removeParticipant:
+                ConversationHandler.removeParticipant(call.argument("participantName"), call.argument("conversationSid"), result);
+                break;
+            // Get & Listen messages from the specific conversation #
+            case Methods.receiveMessages:
+            case Methods.subscribeToMessageUpdate:
+                ConversationHandler.subscribeToMessageUpdate(call.argument("conversationSid"));
+                break;
+            // Get participants from the specific conversation #
+            case Methods.getParticipants:
+                ConversationHandler.getParticipants(call.argument("conversationSid"), result);
+                break;
+            case Methods.unSubscribeToMessageUpdate:
+                ConversationHandler.unSubscribeToMessageUpdate(call.argument("conversationSid"));
+                break;
+            case Methods.updateAccessToken:
+                ConversationHandler.updateAccessToken(call.argument("accessToken"), result);
+                break;
+            case Methods.sendTypingIndicator:
+                ConversationHandler.sendTypingIndicator(call.argument("conversationSid"), result);
+                break;
+            default:
+                break;
+        }
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-    eventChannel.setStreamHandler(null);
-    tokenEventChannel.setStreamHandler(null);
-    participantsEventChannel.setStreamHandler(null);
-  }
-
-  @Override
-  public void onListen(Object arguments, EventSink events) {
-    this.eventSink = events;
-    this.tokenEventSink = events;
-    this.participantsEventSink = events;
-    ConversationHandler conversationHandler = new ConversationHandler();
-    conversationHandler.setListener(this);
-    conversationHandler.setTokenListener(this);
-    conversationHandler.setParticipantListener(this);
-  }
-
-  @Override
-  public void onCancel(Object arguments) {
-    eventSink = null;
-    tokenEventSink = null;
-    participantsEventSink = null;
-  }
-
-  @Override
-  public void onMessageUpdate(Map message) {
-    /// Pass the message result back to the Flutter side
-    if (this.eventSink != null) {
-      this.eventSink.success(message);
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        eventChannel.setStreamHandler(null);
+        tokenEventChannel.setStreamHandler(null);
+        participantsEventChannel.setStreamHandler(null);
     }
-  }
-
-  @Override
-  public void onTypingUpdate(boolean isTyping) {
-    if (this.eventSink != null) {
-      this.eventSink.success(isTyping);
-    }
-  }
-
-  @Override
-  public void onTokenStatusChange(Map message) {
-    /// Pass the message result back to the Flutter side
-    if (this.tokenEventSink != null) {
-      this.tokenEventSink.success(message);
-    }
-  }
-
-  @Override
-  public void onParticipantAdded(String conversationSid, String participantIdentity) {
-    /// Pass the message result back to the Flutter side
-    if (this.participantsEventSink != null) {
-      this.participantsEventSink.success(participantIdentity);
-    }
-  }
 }
